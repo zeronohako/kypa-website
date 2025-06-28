25890ee192bc7750c939e5a8f77c13fbb39ddf91
yunsuhan0107 <46840483+yunsuhan0107@users.noreply.github.com>
2025-06-28 17:04:30 +0900

    fix kr link

--- src/app/kr/page.tsx
+++ src/app/kr/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 // src/app/page.tsx
 import Image from 'next/image'; 
 import Link from 'next/link';
 import ImageCarousel from '@/components/kr/ImageCarousel';
 import ContactBlock from '@/components/kr/ContactBlock';
 import React from 'react'; 
+import { usePathname } from 'next/navigation';
+
 
 export default function HomePage() {
   const carouselImages = [
@@ -11,6 +15,10 @@ export default function HomePage() {
     { src: '/images/carousel-2.jpg', alt: 'image2' },
     { src: '/images/carousel-3.jpg', alt: 'image3' },
   ];
+
+  const pathname = usePathname();
+  const currentLocale = pathname?.split('/')[1] ?? 'en'; // fallback to 'en'
+  const withLocale = (path: string) => `/${currentLocale}${path}`;
 
   return (
     <div>
@@ -30,7 +38,7 @@ export default function HomePage() {
           <p className="text-black text-xl md:text-2xl mb-8">
             피클볼을 통해 어린이와 청소년을 더 건강하게!
           </p>
-          <Link href="/join-us" className="bg-[#52b49b] hover:bg-[#48a58d] text-white font-bold py-3 px-8 rounded-full text-lg transition duration-300">
+          <Link href={withLocale('/join-us')} className="bg-[#52b49b] hover:bg-[#48a58d] text-white font-bold py-3 px-8 rounded-full text-lg transition duration-300">
             지금 가입하기 &#x2192;
           </Link>
         </div>
@@ -43,7 +51,7 @@ export default function HomePage() {
           대한 유소년 피클볼 협회(KYPA)는 2023년, 대한민국에 피클볼을 더 널리 알리고자 설립되었습니다. KYPA는 국내 경기 기획 및 운영, 피클볼 코칭, 소외계층을 위한 피클볼 교육 봉사를 주요 사업으로 하고 있습니다. KYPA 멤버가 되는 조건은 단 하나,대한민국 7-18세의 유소년이면 됩니다. 피클볼을 통해 몸과 마음을 건강하게, 그리고 우리 사회를 더 건강하게 만들어 주세요!
         </p>
         <div className="text-center mt-8">
-          <Link href="/about" className="text-[#40937e] hover:underline text-xl">
+          <Link href={withLocale('/about')} className="text-[#40937e] hover:underline text-xl">
             더 알아보기 &#x2192;
           </Link>
         </div>
